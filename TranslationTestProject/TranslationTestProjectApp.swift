f8860c5088a7cc8b7cce84b080c1052f61405b42
Emanuel Mairoll <office@emanuelmairoll.at>
2021-09-07 16:00:26 +0200

    Initial Commit

---
+++ TranslationTestProject/TranslationTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationTestProjectApp.swift
+//  TranslationTestProject
+//
+//  Created by Emanuel Mairoll on 07.09.21.
+//
+
+import SwiftUI
+
+@main
+struct TranslationTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
